b8b0957ba3f0dac3e347293ac726668ee24f4f97
Jivan Ghadage <jivan.ghadgae@jadeglobal.com>
2016-08-04 17:42:40 +0530

    starting with service

---
+++ app/src/main/java/com/status/callie/Services/ServiceCallie.java
@@ -0,0 +1,23 @@
+package com.status.callie.Services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by jivan.ghadage on 8/4/2016.
+ */
+public class ServiceCallie extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
