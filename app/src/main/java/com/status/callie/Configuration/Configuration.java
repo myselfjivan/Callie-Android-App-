daa49d35646ff52f7ac0d3212f43e14e5b8c7a05
Jivan Ghadage <jivan.ghadgae@jadeglobal.com>
2016-08-04 16:56:34 +0530

    starting with android instance of callie

---
+++ app/src/main/java/com/status/callie/Configuration/Configuration.java
@@ -0,0 +1,8 @@
+package com.status.callie.Configuration;
+
+/**
+ * Created by jivan.ghadage on 8/4/2016.
+ */
+public class Configuration {
+    public static final String URL = "https://192.168.134.128/laravel_otp/public/api";
+}
